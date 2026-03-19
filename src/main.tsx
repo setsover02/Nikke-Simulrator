80ace322498ca5063e8f0606762004adedf3f706
Noah <setsover02@naver.com>
2026-03-19 17:48:14 +0900

    install / app.tsx 삭제 css 단일화

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import Home from './pages/Home'
+import Header from './components/Header'
+import './assets/app.css'
+
+function App() {
+  return (
+    <div style={{ paddingTop: '92px' }}>
+      <Header />
+      <Home />
+    </div>
+  );
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
